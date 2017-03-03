acf364de947f70b1cadd71761abcf7aee08941b0
zhaobingcn <zhaobingcn@hotmail.com>
2017-03-03 14:08:10 +0800

    Initial commit

---
+++ src/main/scala/com/neo4j/spark/connector.scala
@@ -0,0 +1,13 @@
+package com.neo4j.spark
+
+import org.neo4j.spark._
+
+/**
+  * Created by zhzy on 17-3-3.
+  */
+object connector{
+
+  def main(args: Array[String]): Unit = {
+    println("hello scala");
+  }
+}
