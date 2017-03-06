2330bd2b0e80c8e946ee4715b4aac3cd52a62d1d
zhaobingcn <zhaobingcn@hotmail.com>
2017-03-06 15:03:26 +0800

    3.6.15.00

--- src/main/scala/com/neo4j/spark/connector.scala
+++ src/main/scala/com/neo4j/spark/connector.scala
@@ -1,5 +1,6 @@
 package com.neo4j.spark
 
+import org.apache.spark.{SparkConf, SparkContext}
 import org.neo4j.spark._
 
 /**
@@ -8,6 +9,11 @@ import org.neo4j.spark._
 object connector{
 
   def main(args: Array[String]): Unit = {
-    println("hello scala");
+    val conf = new SparkConf().setMaster("local").setAppName("mySpark").set("spark.neo4j.bolt.password","654321")
+    val sc = new SparkContext(conf)
+    val neo = Neo4j(sc)
+    val neordd = neo.cypher("match (n:Person) return n.name").loadRowRdd
+    val pairRDD = neordd.map(x => x(0))
+    pairRDD.foreach(n => println(n))
   }
 }
